57645c3aab2afb5122f030cee78abd03ca87ea54
sylvia_mavuba <smavuba@voyages-sncf.com>
2018-10-04 22:07:20 +0200

    wip

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,81 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
 
 function getTotalPrice() {
+    var price;
+    var quantity;
+    var totalUnit;
+    var total = 0;
+    var products = document.getElementsByClassName('product');
+
+    for (var i = 0; i < products.length; i++) {
+
+        var childrens = products[i].childNodes;
+        for (var j = 0; j < childrens.length; j++) {
+            if (childrens[j].className === 'price-container') {
+                price = childrens[j].firstElementChild.innerText
+            }
+
+            if (childrens[j].className === 'quantity-container') {
+                quantity = childrens[j].lastElementChild.value
+            }
+
+            if (childrens[j].className === 'total-container') {
+                totalUnit = childrens[j].firstElementChild.innerHTML = (price * quantity)
+            }
+
+        }
+        total += totalUnit
+
+    }
+
+    document.getElementById('total-prices').innerHTML = total;
 
 }
 
-function createQuantityInput(){
+function createQuantityInput() {
 
 }
 
-function createDeleteButton(){
+function createDeleteButton() {
 
 }
 
-function createQuantityNode(){
+function createQuantityNode() {
 
 }
 
-function createItemNode(dataType, itemData){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createNewItem(){
+function createNewItem() {
 
 }
 
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+window.onload = function() {
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+    var calculatePriceButton = document.getElementById('calc-prices-button');
+    var createItemButton = document.getElementById('new-item-create');
+    var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+    calculatePriceButton.onclick = getTotalPrice;
+    createItemButton.onclick = createNewItem;
+
+    for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].onclick = deleteItem;
+    }
 };
